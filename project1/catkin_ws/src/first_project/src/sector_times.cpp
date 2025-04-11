95f0eefc838189f08694ef96f3c7562213c9f01d
simocigi <simone.controguerra@mail.polimi.it>
2025-04-11 22:03:36 +0200

    Creato classi per i nodi + funzione per convertire da GPS (da ultimare)

--- project1/catkin_ws/src/first_project/src/sector_times.cpp
+++ project1/catkin_ws/src/first_project/src/sector_times.cpp
@@ -1,5 +1,20 @@
-int main(int argc, char const *argv[])
-{
-	/* code */
+#include "ros/ros.h"
+
+
+class Sector_Times{
+private:
+	ros::NodeHandle n;
+    ros::Publisher pub;
+    ros::Subscriber sub;
+public:
+	void init(){
+		
+	}
+}
+
+int main(int argc, char **argv) {
+	ros::init(argc, argv, "sector_times");
+	Sector_Times sector_times;
+	sector_times.init();
 	return 0;
 }
