26516ef058b47ad05c94db06cefd6284d40be440
g1ack52 <gg.galbiatigiacomo@gmail.com>
2025-04-14 22:47:13 +0200

    aggiunta la funzione per calcolare odometria

--- project1/catkin_ws/src/first_project/src/odometer.cpp
+++ project1/catkin_ws/src/first_project/src/odometer.cpp
@@ -1,29 +1,57 @@
 #include "ros/ros.h"
 #include "math.h"
-# include <nav_msgs/Odometry.h>	
+#include <nav_msgs/Odometry.h>	
+#include <geometry_msgs/Quaternion.h>
+#include <geometry_msgs/PointStamped.h>
 
 class Odometer{
 private:
 	ros::NodeHandle n;
 	ros::Publisher pub;
 	ros::Subscriber sub;
+	ros::Time last_time;
 
-	double alpha; //steering angle from /speedsteer topic [deg]
-	double V; //odometric center velocity from /speedsteer [km/h]
+	const double d = 0.1765; //distance from front to rear wheel
+	double x = 0; y = 0; theta = 0; //valori iniziali
+	
+	void publishOdometry(){
+		nav_msgs::Odometry odometry_msg;
+		geometry_msgs::Quaternion rotation_q = tf::createQuaternionMsgFromYaw(theta);		
+		
+		odometry_msg.pose.pose.position.x = x;
+		odometry_msg.pose.pose.position.y = y;
+		odometry_msg.pose.pose.orientation = rotation_q;
+		
+		odometryPublisher.publish(odometry_msg);
+            	ROS_INFO("odometry message has been published!");
+	}
+	
+	void computeOdometry(const geometry_msgs::PointStamped::constPtr &data){
+		double alpha = data->point.x *3.14/32/180; //steering angle [rad]
+		double V = data->point.y *3.6; //odometric velocity [m/s]
+		double delta_t;
 
-	//output odometry
-	nav_msgs::Odometry odometry_msg;
-
-	void Vhdata_odometry(double alpha_star, double V){
-		//need alpha in [rad] and V in [m/s] !!
-
-		double alpha = alpha_star/32;
-		int d = 1.765; //distance from front to rear wheels [m]
+		ros::Time current_time = ros::Time::now();
+		delta_t = current_time.toSec() - last_time.toSec();
+	
+		//Euler Integration	
+		x = x + V*cos(theta)*delta_t;
+		y = y + V*sin(theta)*delta_t;
+		theta = theta + omega*delta_t;
+		
+		publishOdometry();
+		last_time = current_time;
 
 	}
 public:
 	void init(){
-			
+		odometryPublisher = n.advertise<nav_msgs::Odometry>("odom", 1000);	
+		
+		ROS_INFO("publisher started----------");
+		
+		do{
+			last_time = ros::Time::now();
+		} while(!last_time.isValid());
 	}
 }
 
