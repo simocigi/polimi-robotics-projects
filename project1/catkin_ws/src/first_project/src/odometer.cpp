4965e9a746885dfbecd53005fc729823e53ed13a
g1ack52 <gg.galbiatigiacomo@gmail.com>
2025-04-14 16:38:18 +0200

    implementazione odometry v1

--- project1/catkin_ws/src/first_project/src/odometer.cpp
+++ project1/catkin_ws/src/first_project/src/odometer.cpp
@@ -1,14 +1,29 @@
 #include "ros/ros.h"
-
+#include "math.h"
+# include <nav_msgs/Odometry.h>	
 
 class Odometer{
 private:
 	ros::NodeHandle n;
 	ros::Publisher pub;
 	ros::Subscriber sub;
+
+	double alpha; //steering angle from /speedsteer topic [deg]
+	double V; //odometric center velocity from /speedsteer [km/h]
+
+	//output odometry
+	nav_msgs::Odometry odometry_msg;
+
+	void Vhdata_odometry(double alpha_star, double V){
+		//need alpha in [rad] and V in [m/s] !!
+
+		double alpha = alpha_star/32;
+		int d = 1.765; //distance from front to rear wheels [m]
+
+	}
 public:
 	void init(){
-		
+			
 	}
 }
 
