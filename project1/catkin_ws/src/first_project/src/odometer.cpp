4f930a52c632a76a2e10cae2c662e56209989405
g1ack52 <gg.galbiatigiacomo@gmail.com>
2025-04-27 13:31:32 +0200

    launch file con rviz

--- project1/catkin_ws/src/first_project/src/odometer.cpp
+++ project1/catkin_ws/src/first_project/src/odometer.cpp
@@ -54,8 +54,8 @@ private:
 	void publish_message(double speed, double omega){
 		nav_msgs::Odometry msg;
 		msg.header.stamp = ros::Time::now();
-		msg.header.frame_id = "odom";
-		msg.child_frame_id = "base_link";
+		msg.header.frame_id = "start";
+		msg.child_frame_id = "odom";
 		msg.pose.pose.position.x = x;
 		msg.pose.pose.position.y = y;
 		msg.pose.pose.position.z = 0.0;
@@ -71,7 +71,7 @@ private:
 		tf_tr.setOrigin(tf::Vector3(x, y, 0.0));
 		tf_q.setRPY(0, 0, yaw);
 		tf_tr.setRotation(tf_q);
-		tf_br.sendTransform(tf::StampedTransform(tf_tr, ros::Time::now(), "odom", "base_link"));
+		tf_br.sendTransform(tf::StampedTransform(tf_tr, ros::Time::now(), "start", "odom"));
 		ROS_INFO("Published tf. Position: (%.2f, %.2f, 0.0), Orientation: %.2f", x, y, yaw);
 	}
 	
