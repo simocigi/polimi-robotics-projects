248767ad71487e544b2b0727707c7e06009d8eeb
simocigi <simone.controguerra@mail.polimi.it>
2025-06-01 15:00:39 +0200

    launch per salvare la mappa + piccole modifiche insignificanti

--- project2/catkin_ws/src/second_project/src/convert_odom.cpp
+++ project2/catkin_ws/src/second_project/src/convert_odom.cpp
@@ -17,12 +17,11 @@ private:
 	void convert(const nav_msgs::Odometry::ConstPtr &msg){
 		x = msg->pose.pose.position.x;
 		y = msg->pose.pose.position.y;
-    		tf_tr.setOrigin(tf::Vector3(x, y, 0.0));
+        tf_tr.setOrigin(tf::Vector3(x, y, 0.0));
 		tf::Quaternion tf_q(msg->pose.pose.orientation.x, msg->pose.pose.orientation.y, msg->pose.pose.orientation.z, msg->pose.pose.orientation.w); 		
 		tf_tr.setRotation(tf_q);
 		tf_br.sendTransform(tf::StampedTransform(tf_tr, msg->header.stamp, "odom", "base_link"));
-
-    		//ROS_INFO("TF sent from odom to base_link");
+        //ROS_INFO("TF sent from odom to base_link");
 	}
 public:
 	void init(){
