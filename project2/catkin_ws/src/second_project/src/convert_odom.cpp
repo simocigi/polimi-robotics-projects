24611e0cc21942860c1ed9395d87340d619ea39a
Federicortesi <federico.cortesi.02@gmail.com>
2025-06-01 12:25:53 +0200

    Update convert_odom.cpp

--- project2/catkin_ws/src/second_project/src/convert_odom.cpp
+++ project2/catkin_ws/src/second_project/src/convert_odom.cpp
@@ -12,8 +12,17 @@ private:
 	tf::Quaternion tf_q;
 	geometry_msgs::Quaternion geom_q;
 
-	void convert(const nav_msgs::Odometry::ConstPtr &msg){
+	double x = 0.0, y = 0.0;
 
+	void convert(const nav_msgs::Odometry::ConstPtr &msg){
+		x = msg->pose.pose.position.x;
+		y = msg->pose.pose.position.y;
+    		tf_tr.setOrigin(tf::Vector3(x, y, 0.0));
+		tf::Quaternion tf_q(msg->pose.pose.orientation.x, msg->pose.pose.orientation.y, msg->pose.pose.orientation.z, msg->pose.pose.orientation.w); 		
+		tf_tr.setRotation(tf_q);
+		tf_br.sendTransform(tf::StampedTransform(tf_tr, msg->header.stamp, "odom", "base_link"));
+
+    		//ROS_INFO("TF sent from odom to base_link");
 	}
 public:
 	void init(){
@@ -23,7 +32,7 @@ public:
 };
 
 int main(int argc, char **argv) {
-	ros::init(argc, argv, "mapping");
+	ros::init(argc, argv, "convert_odom");
 	Converter converter;
 	converter.init();
 	return 0;
